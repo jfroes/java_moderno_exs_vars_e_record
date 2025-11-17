1ba7a26308a66f2ee73a85819746c315c83cff6b
José Froes <josepaulo.froes@gmail.com>
2025-11-17 16:29:39 -0300

    Exercicio 05: Instancie e exiba os dados de Pessoa.

---
+++ src/Ex_05.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Ex_05 {
+//    Exercicio 05: Instancie e exiba os dados de Pessoa.
+
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa("José", 29);
+
+        System.out.println(p);
+    }
+}
