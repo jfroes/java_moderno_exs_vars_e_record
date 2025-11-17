4b8ef1220831376045a2d003ddb1eba0d28e48ab
José Froes <josepaulo.froes@gmail.com>
2025-11-17 16:32:23 -0300

    Exercicio 06: Adicione um méthodo dentro do record que retorna uma saudação.

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,8 @@
 //    Exercicio 04: Crie um record chamado Pessoa(String nome, int idade).
-public record Pessoa(String nome, int idade) {}
+public record Pessoa(String nome, int idade) {
+
+//    Exercicio 06: Adicione um méthodo dentro do record que retorna uma saudação.
+    public String saudacao(){
+        return "Olá, eu sou " + this.nome;
+    }
+}
