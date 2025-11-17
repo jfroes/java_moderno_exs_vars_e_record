71c689ba101f06011845fcc008106f1d426723f5
José Froes <josepaulo.froes@gmail.com>
2025-11-17 17:30:12 -0300

    Exercicio 17: Adicione um méthodo no record que retorna “Aprovado/Reprovado”.

--- src/Aluno.java
+++ src/Aluno.java
@@ -5,6 +5,10 @@ import java.util.stream.Collectors;
 //  Exercicio 15: Crie um record Aluno(String nome, double nota) e ordene uma lista por nota.
 public record Aluno(String nome, double nota) {
 
+//    Exercicio 17: Adicione um méthodo no record que retorna “Aprovado/Reprovado”.
+    public String isAprovado(Aluno aluno){
+        return aluno.nota() > 6 ? "Aprovado"  : "Reprovado";
+    }
 
 
 }
