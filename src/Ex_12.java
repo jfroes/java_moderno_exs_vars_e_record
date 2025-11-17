34d1b5915d84cd148e0bed2bd3dfde48921396de
José Froes <josepaulo.froes@gmail.com>
2025-11-17 16:54:54 -0300

    Exercício 12: Faça um record Livro(String titulo, String autor, int paginas) e crie uma lista de livros.

---
+++ src/Ex_12.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Ex_12 {
+    public static void main(String[] args) {
+//        Exercício 12: crie uma lista de livros
+
+        var livros = new ArrayList<Livro>();
+    }
+}
---
+++ src/Livro.java
@@ -0,0 +1,4 @@
+//  Exercício 12: Faça um record Livro(String titulo, String autor, int paginas) e crie uma lista de livros.
+public record Livro(String titulo, String autor, int paginas) {
+}
+
